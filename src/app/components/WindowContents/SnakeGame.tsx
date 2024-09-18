8dbbacd059fdd277a16b91a64212a649a024da3f
txnio <bytxnio@gmail.com>
2024-09-18 17:46:39 +0200

    Again

--- src/app/components/WindowContents/SnakeGame.tsx
+++ src/app/components/WindowContents/SnakeGame.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 
 const GameContainer = styled.div`
