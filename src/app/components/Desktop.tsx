07d13ef379dd6e635494bf892754c91b02d64139
txnio <bytxnio@gmail.com>
2024-09-18 18:05:28 +0200

    Revert "Fixing more problems"
    This reverts commit 14c7bcc20373f71df0b9c9aa2aefa4ab79fdf5b4.

--- src/app/components/Desktop.tsx
+++ src/app/components/Desktop.tsx
@@ -68,16 +68,18 @@ const Desktop: React.FC<DesktopProps> = ({
           window.isOpen && (
             <Window
               key={window.id}
-              window={{
-                ...window,
-                position: windowPositions[window.id] || window.position,
-                size: windowSizes[window.id] || { width: 1200, height: 800 }
-              }}
-              closeWindow={closeWindow}
-              bringToFront={bringToFront}
+              id={window.id}
+              title={window.id}
+              onClose={() => closeWindow(window.id)}
+              onFocus={() => bringToFront(window.id)}
+              zIndex={window.zIndex}
+              position={windowPositions[window.id] || window.position}
+              size={windowSizes[window.id] || { width: 1200, height: 800 }}
               updatePosition={updateWindowPosition}
               updateSize={updateWindowSize}
-            />
+            >
+              {/* Window content */}
+            </Window>
           )
         ))}
       </WindowContainerStyled>
